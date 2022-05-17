3d6dc8cce246bac595347fa3c312b17a3bc1fd05
Saddam K <51265259+IsaacSaddam2@users.noreply.github.com>
2022-05-17 20:10:14 +0530

    simple model and docker compose support added

---
+++ StudentsApi/StudentsApi/Models/AssessmentDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsApi.Models
+{
+    public class AssessmentDetails
+    {
+        public int Id { get; set; }
+        public string Term { get; set; }
+        public string SubjectName { get; set; }
+        public int Marks { get; set; }
+    }
+}
---
+++ StudentsApi/StudentsApi/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public bool IsPromoted { get; set; }
+    }
+}
